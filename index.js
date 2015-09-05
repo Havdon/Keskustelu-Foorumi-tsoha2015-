8165ddefd1957ebf6f3d1c69553643af2d245287
Oskar Koli <koli.oskar@gmail.com>
2015-09-05 12:57:39 +0300

    First version of the docs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ try {
 	require.resolve("./config")
 } catch(e) {
 	console.error("Config file not found! See config.temp.js for the required config data.");
-    process.exit(e.code);
+	process.exit(e.code);
 }
 
 var config = require('./config');
