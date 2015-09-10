dcb9580d5f1aad51cfbf802873c55f5669f3ec7d
Oskar Koli <koli.oskar@gmail.com>
2015-09-10 18:47:14 +0300

    Made app fall back on config.temp.js values, if they werent defined in config.js

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ try {
 	console.error("config.js file not found in project root! Clone config.temp.js and use it as a tempalte for the required data.");
 	process.exit(e.code);
 }
+var extend = require('extend'),
+	config = require('./config'),
+	config_temp = require('./config.temp'),
+	app = require('./src/app');
 
-var config = require('./config');
-var app = require('./src/app');
+// Default to config template values if they are not found in config.
+config = extend(true, config_temp, config);
 
 app.init(config);
